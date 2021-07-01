cac113233cf04eefde631ae658ce25805e01b88c
Fabio Vitalba <fabvit9@gmail.com>
2021-07-01 23:18:55 +0200

    [RFC] Code Styling

--- borealis-webclient/src/Game.test.js
+++ borealis-webclient/src/Game.test.js
@@ -1,4 +1,4 @@
-import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-dom/extend-expect'
 
 // import {
 //   toBeInTheDocument,
@@ -6,25 +6,25 @@ import '@testing-library/jest-dom/extend-expect';
 // } from '@testing-library/jest-dom/matchers'
 // expect.extend({toBeInTheDocument, toHaveClass})
 
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer')
 
 describe('game', () => {
 	test('loads', async () => {
-		let browser = await puppeteer.launch({ headless: true });
-		let page = await browser.newPage();
-		await page.goto('http://localhost:3000');
-		let game = await page.waitForSelector('#game');
-		expect(game).not.toBeNull();
-		let bg = await page.waitForSelector('#bg');
-		expect(bg).toBeVisible();
+		let browser = await puppeteer.launch({ headless: true })
+		let page = await browser.newPage()
+		await page.goto('http://localhost:3000')
+		let game = await page.waitForSelector('#game')
+		expect(game).not.toBeNull()
+		let bg = await page.waitForSelector('#bg')
+		expect(bg).toBeVisible()
 
 		// console.log(bg)
-		// expect().toBeVisible();
-		// expect(page.waitForSelector('#fog')).not.toBeNull();
+		// expect().toBeVisible()
+		// expect(page.waitForSelector('#fog')).not.toBeNull()
 
 
-		// const html = await page.$eval('#game', e => e.innerHTML);
-		// expect(html).toMatch(//);
-		// browser.close();
-	}, 3000);
-});
+		// const html = await page.$eval('#game', e => e.innerHTML)
+		// expect(html).toMatch(//)
+		// browser.close()
+	}, 3000)
+})
