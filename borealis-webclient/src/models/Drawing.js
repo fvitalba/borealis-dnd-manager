25089a7834aea7c8ae0796e59216077732dbc439
Fabio Vitalba <fabvit9@gmail.com>
2021-07-01 22:58:36 +0200

    [RFC] Attempt at refactoring Drawing

---
+++ borealis-webclient/src/models/Drawing.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import drawImage from '../controllers/drawImage.js'
+
+const Drawing = ({ game, ref }) => {
+	const load = () => {
+		const dataUrl = game.map.drawUrl
+		if (dataUrl)
+			return drawImage(dataUrl, 'drawing')
+		else
+			return Promise.resolve()
+	}
+
+	return (
+		<canvas id='drawing' ref={ ref } className='passthrough' />
+	)
+}
+
+export default Drawing;
