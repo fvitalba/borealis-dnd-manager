3ff5fab345eb07e9881ffa0d4bedd9dc7350fb3d
Fabio Vitalba <fabvit9@gmail.com>
2021-07-18 14:35:01 +0200

    [FIX] Fixed UserTool View

--- borealis-webclient/src/views/UserToolView.js
+++ borealis-webclient/src/views/UserToolView.js
@@ -11,9 +11,9 @@ const UserToolView = ({ gameState, setGameState, controlPanelState, setControlPa
 			<ToggleButton title='Share mouse (cursor)' value='&#x1f401;' controlPanelState={ controlPanelState } setControlPanelState={ setControlPanelState } />
 			<input title='Cursor size' value={ gameState.state.cursorSize || '' } onChange={ (e) => setGameInt('cursorSize', e) } type='number' min='0' />
 			<hr />
-			<Button title='Redo as dev' value='&#x1f530;' onClick={ initAsDev() } />
-			<Button title='Copy JSON to clipboard' value='&#x1f46f;' onClick={ copyJson() } />
-			<Button title='Paste JSON from clipboard' value='&#x1f4cb;' onClick={ pasteJson() } />
+			<Button title='Redo as dev' value='&#x1f530;' onClick={ initAsDev } />
+			<Button title='Copy JSON to clipboard' value='&#x1f46f;' onClick={ copyJson } />
+			<Button title='Paste JSON from clipboard' value='&#x1f4cb;' onClick={ pasteJson } />
 		</div>
 	)
 }
