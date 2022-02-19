cc2b365bd36cfacb88158f91787371190c8f44f3
Fabio Vitalba <fabvit9@gmail.com>
2022-02-19 22:30:35 +0100

    [FTR] Updated Map Config Style

--- borealis-webclient/src/views/MapToolView.js
+++ borealis-webclient/src/views/MapToolView.js
@@ -1,15 +1,18 @@
 import MapConfig from '../components/MapConfig'
 import Button from './Button'
+import { ImageAdd } from '@styled-icons/fluentui-system-filled/ImageAdd'
 
 const MapToolView = ({ maps, newMapName, setNewMapName, createMap }) => {
     return (
         <div className='control-panel-submenu'>
             <div>
                 <input placeholder='New map name' onChange={ (e) => setNewMapName(e.target.value) } value={ newMapName } className='control-panel-input' />
-                <Button title='Create new map' value='&#x2795;' onClick={ createMap } disabled={ newMapName === '' } />
-                { maps.map((map) => (
-                    <MapConfig key={ `map${map.$id}` } map={ map } />
-                ))}
+                <Button title='Create new map' value={ <ImageAdd width='30' /> } onClick={ createMap } disabled={ newMapName === '' } />
+                <div className='map-config-collection'>
+                    { maps.map((map) => (
+                        <MapConfig key={ `map${map.$id}` } map={ map } />
+                    ))}
+                </div>
             </div>
         </div>
     )
