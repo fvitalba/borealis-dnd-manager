9d11e41f600ed629150813942b4d6c6f1dafdea2
Fabio Vitalba <fabvit9@gmail.com>
2022-02-19 23:10:20 +0100

    [FTR] Updated Token Config Style

--- borealis-webclient/src/views/GuestTokenConfigView.js
+++ borealis-webclient/src/views/GuestTokenConfigView.js
@@ -1,14 +1,14 @@
 const GuestTokenConfigView = ({ token, onTextChange, onIntegerChange }) => {
     return (
-        <div className='tokenConfig'>
+        <div className='token-config'>
             <input value={ token.name || '' } placeholder='Name' size='8' onChange={ (e) => onTextChange('name', e) } className='control-panel-input' />
             <input value={ token.url || '' } placeholder='Url' size='8' onChange={ (e) => onTextChange('url', e) } className='control-panel-input' />
             wh:
-            <input value={ token.width || '' } placeholder='w' onChange={ (e) => onIntegerChange('width', e) } type='number' step='5' min='0' title='width' className='control-panel-input' />
-            <input value={ token.height || '' } placeholder='h' onChange={ (e) => onIntegerChange('height', e) } type='number' step='5' min='0' title='height' className='control-panel-input' />
+            <input value={ token.width || '' } placeholder='w' onChange={ (e) => onIntegerChange('width', e) } type='number' step='5' min='0' title='width' className='control-panel-input-short' />
+            <input value={ token.height || '' } placeholder='h' onChange={ (e) => onIntegerChange('height', e) } type='number' step='5' min='0' title='height' className='control-panel-input-short' />
             xy:
-            <input value={ token.x || '' } placeholder='x' onChange={ (e) => onIntegerChange('x', e) } type='number' step='5' title='x coord' className='control-panel-input' />
-            <input value={ token.y || '' } placeholder='y' onChange={ (e) => onIntegerChange('y', e) } type='number' step='5' title='y coord' className='control-panel-input' />
+            <input value={ token.x || '' } placeholder='x' onChange={ (e) => onIntegerChange('x', e) } type='number' step='5' title='x coord' className='control-panel-input-short' />
+            <input value={ token.y || '' } placeholder='y' onChange={ (e) => onIntegerChange('y', e) } type='number' step='5' title='y coord' className='control-panel-input-short' />
         </div>
     )
 }
--- borealis-webclient/src/views/TokenToolView.js
+++ borealis-webclient/src/views/TokenToolView.js
@@ -1,14 +1,20 @@
 import TokenConfig from '../components/TokenConfig'
 import Button from './Button'
 
+import { PersonAdd } from '@styled-icons/evaicons-solid/PersonAdd'
+
 const TokenToolView = ({ newTokenUrl, setNewTokenUrl, createToken, tokens }) => {
     return (
         <div className='control-panel-submenu'>
-            <input placeholder='New token url' onChange={ (e) => setNewTokenUrl(e.target.value) } value={ newTokenUrl } className='control-panel-input' />
-            <Button title='Create new token' value='&#x2795;' onClick={ createToken } />
-            { tokens.map((token, $i) => (
-                <TokenConfig key={ `token${$i}` } token={ token } />
-            ))}
+            <input placeholder='New token url' onChange={ (e) => setNewTokenUrl(e.target.value) } value={ newTokenUrl } className='control-panel-input-very-long' />
+            <span className='pl-1 tool-group'>
+                <Button title='Create new token' value={ <PersonAdd width='30' /> } onClick={ createToken } />
+            </span>
+            <div className='token-config-collection'>
+                { tokens.map((token, $i) => (
+                    <TokenConfig key={ `token${$i}` } token={ token } />
+                ))}
+            </div>
         </div>
     )
 }
