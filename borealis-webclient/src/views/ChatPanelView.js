17a32798559adb98a7daaca5dcaa5996bb38d156
Fabio Vitalba <fabvit9@gmail.com>
2022-03-06 14:59:05 +0100

    [FTR] Styled different message types

--- borealis-webclient/src/views/ChatPanelView.js
+++ borealis-webclient/src/views/ChatPanelView.js
@@ -1,6 +1,34 @@
 import Button from './Button'
 import { PlaySolidIcon } from './Icons'
 
+const ChatPanelMessage = ({ message, playerInfo }) => {
+    return (
+        <div key={ message.guid } className='chat-panel-message'>
+            <div className='chat-panel-message-info'>
+                <div className='chat-panel-message-username' >{ message.username }</div>
+                { playerInfo ? <div className='chat-panel-message-player-info'>| { playerInfo }</div> : null }
+            </div>
+            <div className='chat-panel-message-content'>{ message.message }</div>
+        </div>
+    )
+}
+
+const ChatPanelCommand = ({ message }) => {
+    return (
+        <div key={ message.guid } className='chat-panel-command'>
+            <div className='chat-panel-command-content'>{ message.message }</div>
+        </div>
+    )
+}
+
+const ChatPanelError = ({ message }) => {
+    return (
+        <div key={ message.guid } className='chat-panel-error'>
+            <div className='chat-panel-error-content'>{ message.message }</div>
+        </div>
+    )
+}
+
 const ChatPanelView = ({ /* chatPanelHidden, toggleHidden, */ noOfCurrentUsers, /* users, */ currentMessage, changeCurrentMessage, addMessage, chatMessages, inputOnKeyDown }) => {
     const playerInfo = 'lvl. XX Barbarian'
     const sortedChatMessages = chatMessages.sort((a, b) => a.timestamp - b.timestamp)
@@ -11,15 +39,17 @@ const ChatPanelView = ({ /* chatPanelHidden, toggleHidden, */ noOfCurrentUsers, 
                 <div className='chat-panel-header-subtitle'>{ noOfCurrentUsers } player(s) online</div>
             </div>
             <div className='chat-panel-content'>
-                { sortedChatMessages.map((message) =>
-                    <div key={ message.guid } className='chat-panel-message'>
-                        <div className='chat-panel-message-info'>
-                            <div className='chat-panel-message-username' >{ message.username }</div>
-                            { playerInfo ? <div className='chat-panel-message-player-info'>| { playerInfo }</div> : null }
-                        </div>
-                        <div className='chat-panel-message-content'>{ message.message }</div>
-                    </div>
-                )}
+                { sortedChatMessages.map((message) => {
+                    switch (message.typeOfMessage) {
+                    case 'message':
+                        return (<ChatPanelMessage message={ message } playerInfo={ playerInfo } />)
+                    case 'command':
+                        return (<ChatPanelCommand message={ message } />)
+                    case 'error':
+                        return (<ChatPanelError message={ message } />)
+                    default: return null
+                    }
+                })}
                 <div className='chat-panel-input'>
                     <input title='Mesage' placeholder='Type your message...' value={ currentMessage } onChange={ changeCurrentMessage } className='chat-panel-input-input' onKeyDown={ inputOnKeyDown } />
                     <Button title='Push refresh to players' value={ <PlaySolidIcon /> } onClick={ addMessage } />
