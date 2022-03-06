61b78842bb0826b0971714c05c2d8eda7e702ab1
Fabio Vitalba <fabvit9@gmail.com>
2022-03-06 18:33:19 +0100

    [FTR] Added Setup Page for Game

--- borealis-webclient/src/components/GameSetup.js
+++ borealis-webclient/src/components/GameSetup.js
@@ -1,12 +1,75 @@
-import Button from '../views/Button'
+import React, { useState } from 'react'
+import { connect } from 'react-redux'
+import { setGameSettings } from '../reducers/metadataReducer'
+import { setUsername } from '../reducers/settingsReducer'
+import GameSetupView from '../views/GameSetupView'
 
-const GameSetup = () => {
+const initialGameSetupState = () => {
+    return {
+        roomName: '',
+        userName: '',
+        isHost: undefined,
+    }
+}
+
+const GameSetup = ({ setGameSettings, setUsername }) => {
+    const [gameSetupState, setGameSetupState] = useState(initialGameSetupState)
+
+    const onRoomNameChange = (e) => {
+        const newRoomName = e.target.value
+        setGameSetupState({
+            ...gameSetupState,
+            roomName: newRoomName,
+        })
+    }
+
+    const onUserNameChange = (e) => {
+        const newUserName = e.target.value
+        setGameSetupState({
+            ...gameSetupState,
+            userName: newUserName,
+        })
+    }
+
+    const onToggleUserButton = (e) => {
+        const isHost = e.target.innerText === 'DM'
+        const defaultUserName = isHost ? 'DM' : 'PC'
+        if (isHost !== gameSetupState.isHost) {
+            setGameSetupState({
+                ...gameSetupState,
+                isHost: isHost,
+                userName: gameSetupState.userName !== '' ? gameSetupState.userName : defaultUserName,
+            })
+        }
+    }
+
+    const onSubmitSetup = () => {
+        setGameSettings(gameSetupState.isHost, gameSetupState.roomName)
+        setUsername(gameSetupState.userName)
+    }
+
+    const isHost = (gameSetupState.isHost === true)
+    const isPlayer = (gameSetupState.isHost === false)
+    const isSubmitEnabled = ((gameSetupState.isHost !== undefined) && (gameSetupState.roomName !== ''))
+
     return (
-        <div>
-            <Button />
-            <Button />
-        </div>
+        <GameSetupView
+            roomName={ gameSetupState.roomName }
+            onRoomNameChange={ onRoomNameChange }
+            userName={ gameSetupState.userName }
+            onUserNameChange={ onUserNameChange }
+            onToggleUserButton={ onToggleUserButton }
+            onSubmitSetup={ onSubmitSetup }
+            isHost={ isHost }
+            isPlayer={ isPlayer }
+            isSubmitEnabled={ isSubmitEnabled }
+        />
     )
 }
 
-export default GameSetup
+const mapDispatchToProps = {
+    setGameSettings,
+    setUsername,
+}
+
+export default connect(undefined, mapDispatchToProps)(GameSetup)
