9e628e8d21fca4aaf071e190d49a85ffd09c52d3
Fabio Vitalba <fabvit9@gmail.com>
2022-03-14 21:35:08 +0100

    [FTR] Maps may now be moved and zoomed

--- borealis-webclient/src/components/Background.js
+++ borealis-webclient/src/components/Background.js
@@ -1,35 +1,58 @@
-import React, { useState } from 'react'
+import React, { useCallback, useState } from 'react'
 import { connect } from 'react-redux'
 import drawImage from '../controllers/drawImage'
-//import { updateMaps } from '../reducers/gameReducer'
 import Canvas from './Canvas'
 
-const Background = ({ game /*, updateMaps*/ }) => {
+const Background = ({ game }) => {
     const [backgroundSettings, setBackgroundSettings] = useState({
         deltaX: 0,
         deltaY: 0,
         scale: 1.0,
+        isDragging: false,
     })
     const selectedMap = game.maps.filter((map) => map.id === game.mapId)
     const map = selectedMap.length > 0 ? selectedMap[0] : undefined
 
-    const draw = (ctx) => {
+    const draw = useCallback((ctx) => {
         if (!map) {
             return
         }
         drawImage(map.imageUrl, map.name, map.x + backgroundSettings.deltaX, map.y + backgroundSettings.deltaY, map.width * backgroundSettings.scale, map.height * backgroundSettings.scale, ctx)
+    }, [ backgroundSettings, map ])
+
+    const onMouseDown = () => {
+        setBackgroundSettings({
+            ...backgroundSettings,
+            isDragging: true,
+        })
     }
 
-    const onMouseDown = (e) => {
-        console.log('clicked on background')
-    }
-
-    const onMouseUp = (e) => {
-        console.log('released mouse on background')
+    const onMouseUp = () => {
+        setBackgroundSettings({
+            ...backgroundSettings,
+            isDragging: false,
+        })
     }
 
     const onMouseMove = (e) => {
-        console.log('moving mouse', e)
+        if (backgroundSettings.isDragging)
+            setBackgroundSettings({
+                ...backgroundSettings,
+                deltaX: backgroundSettings.deltaX + e.movementX,
+                deltaY: backgroundSettings.deltaY + e.movementY,
+            })
+    }
+
+    const onWheel = (e) => {
+        e.preventDefault()
+        console.log(e.deltaY * -0.01)
+
+        const scale = Math.min(Math.max(.125, (backgroundSettings.scale + e.deltaY * -0.01)), 4)
+        console.log(scale)
+        setBackgroundSettings({
+            ...backgroundSettings,
+            scale: scale,
+        })
     }
 
     return (
@@ -40,7 +63,8 @@ const Background = ({ game /*, updateMaps*/ }) => {
             draw={ draw }
             onMouseUp={ onMouseUp }
             onMouseDown={ onMouseDown }
-            onMouseMove={ onMouseMove } />
+            onMouseMove={ onMouseMove }
+            onWheel={ onWheel } />
     )
 }
 
@@ -51,7 +75,6 @@ const mapStateToProps = (state) => {
 }
 
 const mapDispatchToProps = {
-    //updateMaps,
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Background)
