eaae9f89831980d9eee9aa4ba04dd7aee3469f62
Fabio Vitalba <fabvit9@gmail.com>
2022-03-16 13:12:31 +0100

    [FIX] Attempt at fixing Host indicator on active users

--- borealis-webclient/src/reducers/userReducer.js
+++ borealis-webclient/src/reducers/userReducer.js
@@ -15,25 +15,26 @@ const userTemplate = {
 }
 
 const userReducer = (state = initialUserReducer(), action) => {
-    //TODO: Add isHost to implementation of user in Database
     let newUsers = []
     switch (action.type) {
     case SET_USERS_FROM_API:
         newUsers = action.newUsers.map((actionUser) => {
             const currUser = state.users.filter((stateUser) => stateUser.guid === actionUser.guid)
             if ((currUser.length > 0) && (currUser[0].guid)) {
-                return ({
+                return {
                     ...currUser,
                     lastOnline: actionUser.lastOnline,
                     username: actionUser.userName,
-                })
+                    isHost: actionUser.isHost,
+                }
             } else {
-                return ({
+                return {
                     ...userTemplate,
                     guid: actionUser.guid,
                     lastOnline: actionUser.lastOnline,
                     username: actionUser.userName,
-                })
+                    isHost: actionUser.isHost,
+                }
             }
         })
         return {
