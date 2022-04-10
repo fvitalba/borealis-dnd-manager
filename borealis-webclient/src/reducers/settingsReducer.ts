7346216c47359ad0551e36e82ed2a8f32ce1a371
Fabio Vitalba <fabvit9@gmail.com>
2022-04-10 12:49:52 +0200

    [FTR] Converted Settings reducer to TypeScript

--- borealis-webclient/src/reducers/settingsReducer.ts
+++ borealis-webclient/src/reducers/settingsReducer.ts
@@ -8,16 +8,30 @@ import {
     UPDATE_DELTAXY,
     UPDATE_SCALE
 } from '../redux/constants'
+import ControlTool from '../enums/Tool'
 
-const initialSettingsState = () => {
+interface SettingsState {
+    cursorSize: number,
+    fogOpacity: number,
+    fogRadius: number,
+    drawColor: string,
+    drawSize: number,
+    tool: ControlTool,
+    username: string,
+    shareMouse: boolean,
+    deltaX: number,
+    deltaY: number,
+    scale: number,
+}
+
+const initialSettingsState = (): SettingsState => {
     return {
         cursorSize: 50,
         fogOpacity: 0.5,
         fogRadius: 75,
         drawColor: '#fafafa',
         drawSize: 8,
-        tool: 'move',
-        subtool: undefined,
+        tool: ControlTool.Move,
         username: '',
         shareMouse: false,
         deltaX: 0,
@@ -26,35 +40,49 @@ const initialSettingsState = () => {
     }
 }
 
-const settingsReducer = (state = initialSettingsState(), action) => {
+interface SettingsAction {
+    type: string,
+    cursorSize?: number,
+    fogOpacity?: number,
+    fogRadius?: number,
+    drawColor?: string,
+    drawSize?: number,
+    tool?: ControlTool,
+    username?: string,
+    shareMouse?: boolean,
+    deltaX?: number,
+    deltaY?: number,
+    scale?: number,
+}
+
+const settingsReducer = (state = initialSettingsState(), action: SettingsAction): SettingsState => {
     switch (action.type) {
     case CHANGE_CURSORSIZE:
         return {
             ...state,
-            cursorSize: action.cursorSize,
+            cursorSize: action.cursorSize ? action.cursorSize : state.cursorSize,
         }
     case CHANGE_FOG_SETTINGS:
         return {
             ...state,
-            fogOpacity: action.fogOpacity,
-            fogRadius: action.fogRadius,
+            fogOpacity: action.fogOpacity ? action.fogOpacity : state.fogOpacity,
+            fogRadius: action.fogRadius ? action.fogRadius : state.fogRadius,
         }
     case CHANGE_DRAW_SETTINGS:
         return {
             ...state,
-            drawColor: action.drawColor,
-            drawSize: action.drawSize,
+            drawColor: action.drawColor ? action.drawColor : state.drawColor,
+            drawSize: action.drawSize ? action.drawSize : state.drawSize,
         }
     case CHANGE_TOOL:
         return {
             ...state,
-            tool: action.tool,
-            subtool: action.subtool,
+            tool: action.tool ? action.tool : state.tool,
         }
     case CHANGE_USERNAME:
         return {
             ...state,
-            username: action.username,
+            username: action.username ? action.username : state.username,
         }
     case TOGGLE_MOUSESHARING:
         return {
@@ -64,13 +92,13 @@ const settingsReducer = (state = initialSettingsState(), action) => {
     case UPDATE_DELTAXY:
         return {
             ...state,
-            deltaX: action.deltaX,
-            deltaY: action.deltaY,
+            deltaX: action.deltaX ? action.deltaX : state.deltaX,
+            deltaY: action.deltaY ? action.deltaY : state.deltaY,
         }
     case UPDATE_SCALE:
         return {
             ...state,
-            scale: action.scale,
+            scale: action.scale ? action.scale : state.scale,
         }
     default:
         return state
@@ -78,51 +106,50 @@ const settingsReducer = (state = initialSettingsState(), action) => {
 }
 
 //#region Action Creators
-export const setCursorSize = (newCursorSize) => {
+export const setCursorSize = (newCursorSize: number): SettingsAction => {
     return {
         type: CHANGE_CURSORSIZE,
-        cursorSize: parseInt(newCursorSize),
+        cursorSize: newCursorSize,
     }
 }
 
-export const setFogToolSettings = (newFogOpacity, newFogRadius) => {
+export const setFogToolSettings = (newFogOpacity: number, newFogRadius: number): SettingsAction => {
     return {
         type: CHANGE_FOG_SETTINGS,
         fogOpacity: newFogOpacity,
-        fogRadius: parseInt(newFogRadius),
+        fogRadius: newFogRadius,
     }
 }
 
-export const setDrawToolSettings = (newDrawColor, newDrawSize) => {
+export const setDrawToolSettings = (newDrawColor: string, newDrawSize: number): SettingsAction => {
     return {
         type: CHANGE_DRAW_SETTINGS,
         drawColor: newDrawColor,
-        drawSize: parseInt(newDrawSize),
+        drawSize: newDrawSize,
     }
 }
 
-export const setToolSettings = (newTool, newSubtool) => {
+export const setToolSettings = (newTool: ControlTool): SettingsAction => {
     return {
         type: CHANGE_TOOL,
         tool: newTool,
-        subtool: newSubtool,
     }
 }
 
-export const setUsername = (newUsername) => {
+export const setUsername = (newUsername: string): SettingsAction => {
     return {
         type: CHANGE_USERNAME,
         username: newUsername,
     }
 }
 
-export const toggleMousesharing = () => {
+export const toggleMousesharing = (): SettingsAction=> {
     return {
         type: TOGGLE_MOUSESHARING,
     }
 }
 
-export const updateDeltaXY = (newDeltaX, newDeltaY) => {
+export const updateDeltaXY = (newDeltaX: number, newDeltaY: number): SettingsAction => {
     return {
         type: UPDATE_DELTAXY,
         deltaX: newDeltaX,
@@ -130,7 +157,7 @@ export const updateDeltaXY = (newDeltaX, newDeltaY) => {
     }
 }
 
-export const updateScale = (newScale) => {
+export const updateScale = (newScale: number): SettingsAction => {
     return {
         type: UPDATE_SCALE,
         scale: newScale,
