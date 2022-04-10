de50105b00c2f2600382c7a54f5906b09df376a3
Fabio Vitalba <fabvit9@gmail.com>
2022-04-10 13:24:06 +0200

    [FIX] Fixed ESLint Settings

--- borealis-webclient/src/enums/TokenSize.ts
+++ borealis-webclient/src/enums/TokenSize.ts
@@ -1,10 +1,10 @@
 enum TokenSize {
-    "tiny" = 10,
-    "small" = 25,
-    "medium" = 50,
-    "large" = 75,
-    "huge" = 100,
-    "gargantuan" = 150,
+    'tiny' = 10,
+    'small' = 25,
+    'medium' = 50,
+    'large' = 75,
+    'huge' = 100,
+    'gargantuan' = 150,
 }
 
 export default TokenSize
