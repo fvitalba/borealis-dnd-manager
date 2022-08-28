eb3dd53e0cde08a42b9a84b76c983d6675a75f6d
Fabio Vitalba <fabvit9@gmail.com>
2022-08-28 14:38:04 +0200

    [TST] Refactored existing Tests

--- borealis-server/tests/users.test.ts
+++ borealis-server/tests/users.test.ts
@@ -5,6 +5,7 @@ import app from '../app'
 import User, { IUserSchema } from '../models/user'
 import { initialUsers, initialUsersForAuthentication, newActualUser, newGuestUser } from './testData/initial.users'
 import Session from '../models/session'
+import IIncUser from '../incomingInterfaces/incUser'
 
 const usersGetEndpoint = '/api/v1.0/users'
 const usersPostEndpoint = '/api/v1.0/users'
@@ -100,98 +101,102 @@ describe('POST /users - Register', () => {
         await resetInitialUsers()
     }, 100000)
 
-    it('Register a new Actual User', async () => {
+    const registerUser = async (newUser: IIncUser): Promise<supertest.Test> => {
         const params = {
-            newUser: newActualUser,
+            newUser: newUser,
         }
         const response = await supertest(app)
             .post(usersRegisterEndpoint)
             .send(params)
+        return response
+    }
+
+    const checkRegisterUserSuccessful = async (newUser: IIncUser) => {
+        const response = await registerUser(newUser)
         expect(response.body.error).toBeUndefined()
         expect(response.body.guid).not.toBe('')
+    }
 
-        const response2 = await supertest(app).get(usersGetEndpoint)
+    const checkRegisterUserUnsuccessful = async (newUser: IIncUser) => {
+        const response = await registerUser(newUser)
+        expect(response.body.error).not.toBeUndefined()
+        expect(response.body.guid).toBeUndefined()
+    }
+
+    it('Register a new Actual User', async () => {
+        // Take the newActualUser example to create a new User
+        await checkRegisterUserSuccessful(newActualUser)
+
+        // Check if the number of users now is incremented by 1
+        const response = await supertest(app).get(usersGetEndpoint)
         const activeInitialUsers = initialUsers.filter((initialUser) => initialUser.active)
-        expect(response2.body).toHaveLength(activeInitialUsers.length + 1)
+        expect(response.body).toHaveLength(activeInitialUsers.length + 1)
     }, 100000)
 
     it('Do not register a new Actual User with an existing Email', async () => {
+        // Get a list of users with emails that are already present
+        // Then take that Email and overwrite the newActualUser mail
+        // Then try to create a new User using the existing mail
         const existingUserEmails = initialUsers.filter((existingUser) => (existingUser.email !== '') && (existingUser.active))
-        const params = {
-            newUser: {
-                ...newActualUser,
-                email: existingUserEmails[0].email,
-            },
+        const newUserWithExistingEmail = {
+            ...newActualUser,
+            email: existingUserEmails[0].email,
         }
-        const response = await supertest(app)
-            .post(usersRegisterEndpoint)
-            .send(params)
-        expect(response.body.error).not.toBeUndefined()
-        expect(response.body.guid).toBeUndefined()
+        await checkRegisterUserUnsuccessful(newUserWithExistingEmail)
 
-        const response2 = await supertest(app).get(usersGetEndpoint)
+        // Check that we cannot create a new User with a different Email, but the same Username as someone else
+        const newUserWithExistingName = {
+            ...newActualUser,
+            name: existingUserEmails[0].name,
+        }
+        await checkRegisterUserUnsuccessful(newUserWithExistingName)
+
+        // Check if the number of users now is still the same
+        const response = await supertest(app).get(usersGetEndpoint)
         const activeInitialUsers = initialUsers.filter((initialUser) => initialUser.active)
-        expect(response2.body).toHaveLength(activeInitialUsers.length)
+        expect(response.body).toHaveLength(activeInitialUsers.length)
     }, 100000)
 
     it('Do not register a new Actual User without secret or email', async () => {
-        const params = {
-            newUser: {
-                ...newActualUser,
-                email: '',
-            },
+        // Try registering without Email
+        const newUserNoEmail = {
+            ...newActualUser,
+            email: '',
         }
-        const response = await supertest(app)
-            .post(usersRegisterEndpoint)
-            .send(params)
-        expect(response.body.error).not.toBeUndefined()
-        expect(response.body.guid).toBeUndefined()
+        await checkRegisterUserUnsuccessful(newUserNoEmail)
 
-        const params2 = {
-            newUser: {
-                ...newActualUser,
-                secret: '',
-            },
+        // Try registering without secret
+        const newUserNoSecret = {
+            ...newActualUser,
+            secret: '',
         }
-        const response2 = await supertest(app)
-            .post(usersRegisterEndpoint)
-            .send(params2)
-        expect(response2.body.error).not.toBeUndefined()
-        expect(response2.body.guid).toBeUndefined()
+        await checkRegisterUserUnsuccessful(newUserNoSecret)
 
-        const response3 = await supertest(app).get(usersGetEndpoint)
+        // Verify that the list of users is still the same length
+        const response = await supertest(app).get(usersGetEndpoint)
         const activeInitialUsers = initialUsers.filter((initialUser) => initialUser.active)
-        expect(response3.body).toHaveLength(activeInitialUsers.length)
+        expect(response.body).toHaveLength(activeInitialUsers.length)
     }, 100000)
 
     it('Register a new Guest User', async () => {
-        const params = {
-            newUser: newGuestUser,
-        }
-        const response = await supertest(app)
-            .post(usersRegisterEndpoint)
-            .send(params)
-        expect(response.body.error).toBeUndefined()
-        expect(response.body.guid).not.toBe('')
+        // Register a Guest user using the newGuestUser template
+        await checkRegisterUserSuccessful(newGuestUser)
 
+        // Check if the count of users in the DB has correctly increased by 1
         const response2 = await supertest(app).get(usersGetEndpoint)
         const activeInitialUsers = initialUsers.filter((initialUser) => initialUser.active)
         expect(response2.body).toHaveLength(activeInitialUsers.length + 1)
     }, 100000)
 
     it('Do not register a new Guest User with the same UserName as another User', async () => {
-        const params = {
-            newUser: {
-                ...newGuestUser,
-                userName: initialUsers[0].name,
-            },
+        // Try registering a Guest User, using an already existing UserName
+        const newUser = {
+            ...newGuestUser,
+            userName: initialUsers[0].name,
         }
-        const response = await supertest(app)
-            .post(usersRegisterEndpoint)
-            .send(params)
-        expect(response.body.error).not.toBeUndefined()
-        expect(response.body.guid).toBeUndefined()
+        await checkRegisterUserUnsuccessful(newUser)
 
+        // Check if the User List is still the same length as before
         const response2 = await supertest(app).get(usersGetEndpoint)
         const activeInitialUsers = initialUsers.filter((initialUser) => initialUser.active)
         expect(response2.body).toHaveLength(activeInitialUsers.length)
@@ -203,116 +208,69 @@ describe('POST /users - Authenticate', () => {
         await resetInitialAuthUsers()
     }, 100000)
 
-    it('Authenticate the third Actual User', async () => {
-        // First we authenticate providing all possible parameters
+    const authenticateUser = async (isGuest: boolean, userGuid: string, userName: string, email: string, secret: string): Promise<supertest.Test> => {
         const params = {
-            isGuest: initialUsersForAuthentication[2].guest,
-            userGuid: initialUsersForAuthentication[2].guid,
-            userName: initialUsersForAuthentication[2].name,
-            email: initialUsersForAuthentication[2].email,
-            secret: initialUsersForAuthentication[2].secret,
+            isGuest: isGuest,
+            userGuid: userGuid,
+            userName: userName,
+            email: email,
+            secret: secret,
         }
         const response = await supertest(app)
             .post(usersAuthenticateEndpoint)
             .send(params)
+        return response
+    }
+
+    const checkAuthenticateUserSuccessful = async (isGuest: boolean, userGuid: string, userName: string, email: string, secret: string) => {
+        const response = await authenticateUser(isGuest, userGuid, userName, email, secret)
         expect(response.body.error).toBeUndefined()
-        expect(response.body.guid).not.toBe('')
-        expect(response.body.name).toBe(initialUsersForAuthentication[2].name)
-        expect(response.body.email).toBe(initialUsersForAuthentication[2].email)
-        expect(response.body.guest).toBe(initialUsersForAuthentication[2].guest)
+        if (userGuid !== '')
+            expect(response.body.guid).toBe(userGuid)
+        else
+            expect(response.body.guid).not.toBe('')
+        if (userName !== '')
+            expect(response.body.name).toBe(userName)
+        else
+            expect(response.body.name).not.toBe('')
+        if (email !== '')
+            expect(response.body.email).toBe(email)
+        else
+            expect(response.body.email).not.toBe('')
+        expect(response.body.guest).toBe(isGuest)
         expect(response.body.secret).toBe('')
+    }
 
-        // Then we authenticate by just providing guest, name and secret
-        const params2 = {
-            isGuest: initialUsersForAuthentication[2].guest,
-            userGuid: '',
-            userName: initialUsersForAuthentication[2].name,
-            email: '',
-            secret: initialUsersForAuthentication[2].secret,
-        }
-        const response2 = await supertest(app)
-            .post(usersAuthenticateEndpoint)
-            .send(params2)
-        expect(response2.body.error).toBeUndefined()
-        expect(response2.body.guid).not.toBe('')
-        expect(response2.body.name).toBe(initialUsersForAuthentication[2].name)
-        expect(response2.body.email).not.toBe('')
-        expect(response2.body.guest).toBe(initialUsersForAuthentication[2].guest)
-        expect(response2.body.secret).toBe('')
-
-        // Then we authenticate by just providing guest, mail and secret
-        const params3 = {
-            isGuest: initialUsersForAuthentication[2].guest,
-            userGuid: '',
-            userName: '',
-            email: initialUsersForAuthentication[2].email,
-            secret: initialUsersForAuthentication[2].secret,
-        }
-        const response3 = await supertest(app)
-            .post(usersAuthenticateEndpoint)
-            .send(params3)
-        expect(response3.body.error).toBeUndefined()
-        expect(response3.body.guid).not.toBe('')
-        expect(response3.body.name).not.toBe('')
-        expect(response3.body.email).toBe(initialUsersForAuthentication[2].email)
-        expect(response3.body.guest).toBe(initialUsersForAuthentication[2].guest)
-        expect(response3.body.secret).toBe('')
-    }, 100000)
-
-    it('Do not allow authentication for the new Actual User with the wrong secret', async () => {
-        // First we just provide no secret.
-        const params = {
-            isGuest: initialUsersForAuthentication[2].guest,
-            userGuid: initialUsersForAuthentication[2].guid,
-            userName: initialUsersForAuthentication[2].name,
-            email: initialUsersForAuthentication[2].email,
-            secret: '',
-        }
-        const response = await supertest(app)
-            .post(usersAuthenticateEndpoint)
-            .send(params)
+    const checkAuthenticateUserUnsuccessful = async (isGuest: boolean, userGuid: string, userName: string, email: string, secret: string) => {
+        const response = await authenticateUser(isGuest, userGuid, userName, email, secret)
         expect(response.body.error).not.toBeUndefined()
         expect(response.body.guid).toBeUndefined()
         expect(response.body.name).toBeUndefined()
         expect(response.body.email).toBeUndefined()
         expect(response.body.guest).toBeUndefined()
         expect(response.body.secret).toBeUndefined()
+    }
+
+    it('Authenticate the third Actual User', async () => {
+        // First we authenticate providing all possible parameters
+        await checkAuthenticateUserSuccessful(initialUsersForAuthentication[2].guest, initialUsersForAuthentication[2].guid, initialUsersForAuthentication[2].name, initialUsersForAuthentication[2].email, initialUsersForAuthentication[2].secret)
+
+        // Then we authenticate by just providing guest, name and secret
+        await checkAuthenticateUserSuccessful(initialUsersForAuthentication[2].guest, '', initialUsersForAuthentication[2].name, '', initialUsersForAuthentication[2].secret)
+
+        // Then we authenticate by just providing guest, mail and secret
+        await checkAuthenticateUserSuccessful(initialUsersForAuthentication[2].guest, '', '', initialUsersForAuthentication[2].email, initialUsersForAuthentication[2].secret)
+    }, 100000)
+
+    it('Do not allow authentication for the new Actual User with the wrong secret', async () => {
+        // First we just provide no secret.
+        await checkAuthenticateUserUnsuccessful(initialUsersForAuthentication[2].guest, initialUsersForAuthentication[2].guid, initialUsersForAuthentication[2].name, initialUsersForAuthentication[2].email, '')
 
         // Then we provide just a username and nothing else
-        const params2 = {
-            isGuest: false,
-            userGuid: '',
-            userName: initialUsersForAuthentication[2].name,
-            email: '',
-            secret: '',
-        }
-        const response2 = await supertest(app)
-            .post(usersAuthenticateEndpoint)
-            .send(params2)
-        expect(response2.body.error).not.toBeUndefined()
-        expect(response2.body.guid).toBeUndefined()
-        expect(response2.body.name).toBeUndefined()
-        expect(response2.body.email).toBeUndefined()
-        expect(response2.body.guest).toBeUndefined()
-        expect(response2.body.secret).toBeUndefined()
+        await checkAuthenticateUserUnsuccessful(false, '', initialUsersForAuthentication[2].name, '', '')
 
         // Then we provide just a username and the already hashed secret
-        const params3 = {
-            isGuest: false,
-            userGuid: '',
-            userName: initialUsersForAuthentication[2].name,
-            email: '',
-            secret: getHashedSecret(initialUsersForAuthentication[2].secret),
-        }
-        const response3 = await supertest(app)
-            .post(usersAuthenticateEndpoint)
-            .send(params3)
-        expect(response3.body.error).not.toBeUndefined()
-        expect(response3.body.guid).toBeUndefined()
-        expect(response3.body.name).toBeUndefined()
-        expect(response3.body.email).toBeUndefined()
-        expect(response3.body.guest).toBeUndefined()
-        expect(response3.body.secret).toBeUndefined()
+        await checkAuthenticateUserUnsuccessful(false, '', initialUsersForAuthentication[2].name, '', getHashedSecret(initialUsersForAuthentication[2].secret))
     }, 100000)
 
 })
@@ -352,23 +310,31 @@ describe('POST /users - Sessions', () => {
     }
 
     it('Start a Session for an Actual User', async () => {
+        // Provide a normal actual user and authenticate for a brand new Session
         await checkStartSessionSuccessful(initialUsersForAuthentication[2].guest, initialUsersForAuthentication[2].guid, initialUsersForAuthentication[2].secret, '')
     }, 100000)
 
     it('Get the Active Session for an Actual User', async () => {
+        // Create a new Session
         const activeSessionId = await checkStartSessionSuccessful(initialUsersForAuthentication[2].guest, initialUsersForAuthentication[2].guid, initialUsersForAuthentication[2].secret, '')
+        // Take the earlier created session and check if you get the same session or a new one
         const newSessionId = await checkStartSessionSuccessful(initialUsersForAuthentication[2].guest, initialUsersForAuthentication[2].guid, initialUsersForAuthentication[2].secret, activeSessionId)
         expect(activeSessionId).toBe(newSessionId)
     }, 100000)
 
     it('Start a Session for a Guest User', async () => {
+        // Start a session for a guest user
         await checkStartSessionSuccessful(initialUsersForAuthentication[4].guest, initialUsersForAuthentication[4].guid, initialUsersForAuthentication[4].secret, '')
     }, 100000)
 
     it('Do not start a Session for a nonexisting User', async () => {
+        // Try starting a session without any information
         await checkStartSessionUnsuccessful(false, '', '', '')
+        // Try starting a session with a random username and secret
         await checkStartSessionUnsuccessful(false, randomUUID(), randomUUID(), '')
+        // Try starting a session for a guest user, but without providing a name or existing token
         await checkStartSessionUnsuccessful(true, '', '', '')
+        // Try starting a session for a guest user, that does not exist (random name).
         await checkStartSessionUnsuccessful(true, randomUUID(), '', '')
     }, 100000)
 
